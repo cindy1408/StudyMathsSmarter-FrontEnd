01959f4129b1fa735a11ba1fa29a54c17fccbf77
Yulin Chen <elim2015@live.com>
2021-10-08 15:05:57 +0100

    Quiz connection testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import UserContainer from './containers/UserContainer';
 import QuestionContainer from './containers/QuestionContainer';
-
+import QuizContainer from 'containers/QuizContainer';
 function App() {
   return (
     <div className="App">
       {/* <UserContainer /> */}
       <QuestionContainer />
+      <QuizContainer />
     </div>
   );
 }
