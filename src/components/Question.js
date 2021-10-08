e403441fc5a63e9202c05be96658c522b95132db
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2021-10-08 12:10:22 +0100

    display img from db

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,10 @@
 
 const Question = (props) => {
-    var base64Image = `data:image/png;base64, ${props.question.questionImage}`;
     return(
         <div className='question' id={props.id}>
             <h3>Topics: {JSON.stringify(props.question)}</h3>
-            <h3>{JSON.stringify(props.question.topic)}</h3>  
-            {/* <h3>{props.question.topic}</h3>     */}
-            <img src={{url: base64Image}} alt="Questions" /> 
+            <h3>{JSON.stringify(props.question.topic)}</h3> 
+            <img src= {"data:image/png;base64," + props.question.questionImage}  alt="Questions" /> 
         </div>
     )
 }
