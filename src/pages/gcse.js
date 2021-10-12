788d8366f8e18fe781ddae564a56d5b870b9e2ae
Jolan1 <jnrewaju1@gmail.com>
2021-10-12 15:52:39 +0100

    testing

--- src/pages/gcse.js
+++ src/pages/gcse.js
@@ -4,14 +4,10 @@ import {BannerGcse} from "../components/cards.js"
 
 
 function gcse(){
-<<<<<<< HEAD
     return (
         <BannerGcse title="This is the GCSE page" subText="Yes still GCSE page" />
         )
-=======
-<<<<<<< HEAD
     return ("Hello")
-=======
     return (
     <div>
     <p >Feel free to drop your thoughts! Whether you want to drop a nice compliment to another member of staff or if you would like to leave some constructive feedback. 
@@ -28,8 +24,5 @@ function gcse(){
     
 
     )
-
->>>>>>> ada738768dc9d5045201fc6aa6de2593f0a756dc
->>>>>>> dd7a5df9f71f1a162315d8cbc87d625a5bfece9f
 }
 export default gcse;
