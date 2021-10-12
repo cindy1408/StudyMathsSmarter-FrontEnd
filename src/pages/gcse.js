076d7383ad109fd359b5a4db6b11caf9812f0dfe
Jolan1 <jnrewaju1@gmail.com>
2021-10-12 16:36:59 +0100

    test

--- src/pages/gcse.js
+++ src/pages/gcse.js
@@ -4,33 +4,9 @@ import {BannerGcse} from "../components/cards.js"
 
 
 function gcse(){
-<<<<<<< HEAD
-    return (
-        <BannerGcse title="This is the GCSE page" subText="Yes still GCSE page" />
-        )
-    return ("Hello")
-    return (
-    <div>
-    <p >Feel free to drop your thoughts! Whether you want to drop a nice compliment to another member of staff or if you would like to leave some constructive feedback. 
-    Entries only last for a limited time and are anonymous.</p>
-    <p >Feel free to drop your thoughts! Whether you want to drop a nice compliment to another member of staff or if you would like to leave some constructive feedback. 
-    Entries only last for a limited time and are anonymous.</p>
-    <p >Feel free to drop your thoughts! Whether you want to drop a nice compliment to another member of staff or if you would like to leave some constructive feedback. 
-    Entries only last for a limited time and are anonymous.</p>
-    <p >Feel free to drop your thoughts! Whether you want to drop a nice compliment to another member of staff or if you would like to leave some constructive feedback. 
-    Entries only last for a limited time and are anonymous.</p>
-
-
-    </div>
-    
-
-    )
-}
-=======
 
     return (
         <BannerGcse title="This is the GCSE page" subText="Yes still GCSE page" />
         )
     }
->>>>>>> 6fc71e1e3f2ed07a9a25191dc529f6e1f118421c
 export default gcse;
