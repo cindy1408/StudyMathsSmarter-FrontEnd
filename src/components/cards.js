41702317259284745c51efcaa3b5c8ffc6e3793f
Bianca <marcucci.bianca96@gmail.com>
2021-10-12 17:40:36 +0100

    added card

--- src/components/cards.js
+++ src/components/cards.js
@@ -20,8 +20,21 @@ function BannerGcse (props){
     )
 }
 
+function MainBody (props){
+    return(
+        <div className="mainBody">
+            <img className="mainBodyImage" src={props.imageSrc}/>
+            <div className="mainBodyContent">
+            <h1 className="mainBodyTitle">{props.title}</h1>
+            <h3 className="mainBodySubText">{props.subText}</h3>
+            </div>
+        </div>
+        
+    )
+}
+
     
 
 export{
-    Banner, BannerGcse,
+    Banner, BannerGcse, MainBody,
 };
