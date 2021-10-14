88ea93c351cadb8d0184d546a7505eee5f7bfc00
Jolan1 <jnrewaju1@gmail.com>
2021-10-14 18:38:35 +0100

    pull from bianca3

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,18 +2,6 @@ import "./cards.css";
 import React from "react";
 import { useHistory } from "react-router";
 
-<<<<<<< HEAD
-function Banner (props){
-    return(
-        <div className="banner">
-            <div className="bannerContent">
-            <h1 className="bannerTitle">{props.title}</h1>
-            <h3 className="bannerSubText">{props.subText}</h3>
-            </div>
-        </div>
-        
-    )
-=======
 function Banner(props) {
   return (
     <div className="banner">
@@ -23,7 +11,6 @@ function Banner(props) {
       </div>
     </div>
   );
->>>>>>> b43c461649c6619908f44f795093f6b4334ca351
 }
 function BannerGcse(props) {
   return (
@@ -82,23 +69,6 @@ function MainBody2 (props){
     )
 }
 
-<<<<<<< HEAD
-function MainBody3 (props){
-    return(
-        <div className="mainBody3">
-            <div className="mainBodyContent3">
-            <h1 className="mainBodyTitle3">{props.title}</h1>
-            <button class="startButton"  ><span>Start </span></button>
-            </div>
-        </div>
-        
-    )
-}
-
-export{
-    Banner, BannerGcse, MainBody, MainBody2,MainBody3
-};
-=======
 function MainBody3(props) {
   const history = useHistory();
   function RedirectGcse() {
@@ -119,4 +89,3 @@ function MainBody3(props) {
 }
 
 export { Banner, BannerGcse, MainBody, MainBody2, MainBody3, MainBody4 };
->>>>>>> b43c461649c6619908f44f795093f6b4334ca351
