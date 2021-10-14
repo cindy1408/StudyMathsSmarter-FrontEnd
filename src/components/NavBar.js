58a896ae8b405104212a50c96408728ab0fb199e
Bianca <marcucci.bianca96@gmail.com>
2021-10-14 18:06:21 +0100

    more styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,62 +1,39 @@
 import React from "react";
 import "./NavBar.css";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
 
 function NavBar() {
   return (
-
     <div>
-
-
-    <nav className="navbar navbar-expand-lg navbar-light">
-  
-  
-  <div class=" centerContentNav navbar-collapse" id="">
-  <img className="logo" src='./resources/logo.png'/>
-    <ul class="navbar-nav ">
-    <Link className="nav-link" to="/homePage">
-      <li class="fontNav nav-item active navBarStyling">
-         HOME
-      </li>
-      </Link>
-      <Link className="nav-link" to="/GCSEStart">
-      <li class="fontNav nav-item active navBarStyling">
-         GCSE
-      </li>
-      </Link>
-      <Link className="nav-link" to="/comingSoon">
-      <li class="fontNav nav-item active navBarStyling">
-         IGCSE
-      </li>
-      </Link>
-      <Link className="nav-link" to="/comingSoon">
-      <li class="fontNav nav-item active navBarStyling">
-         AS
-      </li>
-      </Link>
-      <Link className="nav-link" to="/comingSoon">
-      <li class="fontNav nav-item active navBarStyling">
-         A-LEVEL
-      </li>
-      </Link>   
-      <Link className="nav-link" to="/login">
-      <li class="fontNav nav-item active navBarStyling">
-     LOGIN
-      </li>
-      </Link>     
-      </ul>
-      
-      
-    
-  </div>
- 
-</nav>
-{/*<p>I am always here</p>*/}
-</div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div class=" centerContentNav navbar-collapse" id="">
+          <img className="logo" src="./resources/logo.png" />
+          <ul class="navbar-nav ">
+            <Link className="nav-link" to="/homePage">
+              <li class="fontNav nav-item active navBarStyling">HOME</li>
+            </Link>
+            <Link className="nav-link" to="/GCSEStart">
+              <li class="fontNav nav-item active navBarStyling">GCSE</li>
+            </Link>
+            <Link className="nav-link" to="/comingSoon">
+              <li class="fontNav nav-item active navBarStyling">IGCSE</li>
+            </Link>
+            <Link className="nav-link" to="/comingSoon">
+              <li class="fontNav nav-item active navBarStyling">AS</li>
+            </Link>
+            <Link className="nav-link" to="/comingSoon">
+              <li class="fontNav nav-item active navBarStyling">A-LEVEL</li>
+            </Link>
+            <Link className="nav-link" to="/login">
+              <li class="fontNav nav-item active navBarStyling">LOGIN</li>
+            </Link>
+          </ul>
+        </div>
+      </nav>
+      {/*<p>I am always here</p>*/}
+    </div>
   );
-  }
+}
 
 export default NavBar;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,31 +1,50 @@
 // import { logDOM } from "@testing-library/react";
 import React from "react";
-import {Banner, MainBody,MainBody2,MainBody3,} from "../components/cards.js";
-import PupilTeacher from '../images/FR_English.png';
-import GCSEMath from '../images/gcseMathPaper.png';
-import Smile from '../images/logo3.png';
+import {
+  Banner,
+  MainBody,
+  MainBody2,
+  MainBody3,
+  MainBody4,
+} from "../components/cards.js";
+import ALevelsCard from "../images/animated2.png";
+import GCSECard from "../images/animated3.png";
 import "./homePage.css";
 
-function homePage(){
-    return (
-       <>
-      <Banner title="Interactive maths learning for all students" subText="Providing complete curriculum coverage from GCSE to A Level, decodeMaths offers quizzes, revision packs, along with a wealth of resources that will help you develop your confidence and fluency in maths."/>
+function homePage() {
+  return (
+    <>
+      <Banner
+        title="Interactive maths learning for all students"
+        subText="Providing complete curriculum coverage from GCSE to A Level, decodeMaths offers quizzes, revision packs, along with a wealth of resources that will help you develop your confidence and fluency in maths."
+      />
       <div className="homePage">
-            <MainBody imageSrc={GCSEMath} title="GCSE " subText="Our GCSE section includes a quiz which is a great tool for revising for any upcoming exams. The decodeMaths revision packs are designed to help students self-serve their independent learning. Packs include a curated set of revision lessons, homework tasks, and online worksheets that offer targeted revision in the run up to exams."/>
-            <MainBody2  title="Testimonials " subText="Using the decodeMaths website is helping our pupils to improve their final scores, and they are enjoying the rapid feedback of the automatic marking. It motivates the less able and offers real challenge to the high-flyers. 
+        <MainBody
+          imageSrc={GCSECard}
+          title="GCSE "
+          subText="Our GCSE section includes a quiz which is a great tool for revising for any upcoming exams. The decodeMaths revision packs are designed to help students self-serve their independent learning. Packs include a curated set of revision lessons, homework tasks, and online worksheets that offer targeted revision in the run up to exams."
+        />
+        <MainBody2
+          title="Testimonials "
+          subText="Using the decodeMaths website is helping our pupils to improve their final scores, and they are enjoying the rapid feedback of the automatic marking. It motivates the less able and offers real challenge to the high-flyers. 
 
 Shelley Squadratics, Headteacher at Pi Squared Secondary School
-"/>
-            <MainBody imageSrc={PupilTeacher} title="A-Levels " subText="Our A-Level section includes a quiz which is a great tool for revising for any upcoming AS/A-Level mocks or exams. The decodeMaths revision packs are designed to help students self-serve their independent learning. Packs include a curated set of revision lessons, homework tasks, and online worksheets that offer targeted revision in the run up to exams.
-"/>
-            <MainBody3  title="Ready to study maths smarter? " subText="Try our Quiz"/>
-          
-            
-      <div className="homePage"></div>
-</div>
+"
+        />
+        <MainBody4
+          imageSrc={ALevelsCard}
+          title="A-Levels "
+          subText="Our A-Level section includes a quiz which is a great tool for revising for any upcoming AS/A-Level mocks or exams. The decodeMaths revision packs are designed to help students self-serve their independent learning. Packs include a curated set of revision lessons, homework tasks, and online worksheets that offer targeted revision in the run up to exams.
+"
+        />
+        <MainBody3
+          title="Ready to study maths smarter? "
+          subText="Try our Quiz"
+        />
 
-      
-      </>
-)
+        <div className="homePage"></div>
+      </div>
+    </>
+  );
 }
 export default homePage;
