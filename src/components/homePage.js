078e5d8bf5dc0412509821b357e48ff427104e86
Bianca <marcucci.bianca96@gmail.com>
2021-10-14 16:11:16 +0100

    .

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -11,7 +11,7 @@ function homePage(){
        <>
       <Banner title="Interactive maths learning for all students" subText="Providing complete curriculum coverage from GCSE to A Level, decodeMaths offers quizzes, revision packs, along with a wealth of resources that will help you develop your confidence and fluency in maths."/>
       <div className="homePage">
-            <MainBody imageSrc={GCSEMath} title="GCSE " subText="Check back frequently for the latest COVID-19 travel and specific testing requirements for your destination, including any stops for connecting flights. You are required to come to the airport with the appropriate travel documents.'"/>
+            <MainBody imageSrc={GCSEMath} title="GCSE " subText="Our GCSE section includes a quiz which is a great tool for revising for any upcoming exams. The decodeMaths revision packs are designed to help students self-serve their independent learning. Packs include a curated set of revision lessons, homework tasks, and online worksheets that offer targeted revision in the run up to exams."/>
             <MainBody2  title="Testimonials " subText="SO FUN IH IH"/>
             <MainBody imageSrc={PupilTeacher} title="A-Levels " subText="Check back frequently for the latest COVID-19 travel and specific testing requirements for your destination, including any stops for connecting flights. You are required to come to the airport with the appropriate travel documents.'"/>
             <MainBody3  title="Ready to study maths smarter? " subText="Try our Quiz"/>
